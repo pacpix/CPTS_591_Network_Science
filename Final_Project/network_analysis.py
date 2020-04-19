d1aa57e22d1d3ead8f9afbfd7a7b8dbca77e5ceb
pacpix <julia_stachofsky@protonmail.com>
2020-04-18 23:12:07 -0700

    Complete refactor to non-bipartite algorithms.  Dataset violated assumptions of bipartite graph.  Also changed directionality of technical->technical edges

--- Final_Project/network_analysis.py
+++ Final_Project/network_analysis.py
@@ -9,7 +9,6 @@ Analyze network from generated gml file
 # Third Party Libraries
 import matplotlib.pyplot as plt
 import networkx as nx
-from networkx.algorithms import bipartite
 import numpy as np
 
 # Filenames
@@ -22,26 +21,21 @@ def main():
     # Read in file created with create_gml_dataset.py
     st_graph = nx.read_gml(gml_file)
 
-    # Since graph is directed, need to create two sets of nodes manually
-    # Most bipartite algorithms require passing a nodeset with the graph
-    top_nodes = {n for n, d in st_graph.nodes(
-        data=True) if d['bipartite'] == 0}
-    bottom_nodes = set(st_graph) - top_nodes
+    # Get node subsets 
+    social_nodes = {n for n, d in st_graph.nodes(data=True) if d['repo']==0}
+    technical_nodes = set(st_graph) - social_nodes
 
     # Calculate statistics for network
-    basic_algorithms(st_graph, bottom_nodes)
-    centrality_algorithms(st_graph, bottom_nodes)
-    print('Spectral Bipartivity: ', bipartite.spectral_bipartivity(st_graph))
-    print('Robins Alexander Bipartite Clustering Coeficient: ',
-          bipartite.robins_alexander_clustering(st_graph))
+    basic_algorithms(st_graph)
+    centrality_algorithms(st_graph)
 
     # Create degree distributions and visualize network
-    plot_degree_distributions(st_graph, bottom_nodes)
-    visualize_network(st_graph, top_nodes, bottom_nodes)
+    #plot_degree_distributions(st_graph, social_nodes, technical_nodes)
+    visualize_network(st_graph, social_nodes, technical_nodes)
 
 
 # Basic statistics about the network
-def basic_algorithms(graph, node_set):
+def basic_algorithms(graph):
 
     # Calculate number of nodes (n)
     print('Nodes: ', len(graph))
@@ -54,59 +48,75 @@ def basic_algorithms(graph, node_set):
           nx.number_weakly_connected_components(graph))
     # Calculate average path length (l)
     print('Average Path Length: ', nx.average_shortest_path_length(graph))
+    # Calculate maximum degree (d)
+    degree_list = [lis[1] for lis in nx.degree(graph)]
+    print('Maximum Degree: ', max(degree_list))
     # Calculate density of graph
-    print('Density: ', bipartite.density(graph, node_set))
+    print('Density: ', nx.density(graph))
 
 
 # Centrality statistics about the network
-def centrality_algorithms(graph, node_set):
+def centrality_algorithms(graph):
 
     # Centrality functions return a dictionary of values
     # Calculate the maximum and print node name with value
     # Value stays the same for closness centrality, but the node itself changes
-    centrality_dict = bipartite.closeness_centrality(graph, node_set)
-    print('Closeness Centrality: ', max(centrality_dict,
-                                        key=centrality_dict.get), max(centrality_dict.values()))
-    centrality_dict = bipartite.degree_centrality(graph, node_set)
+    centrality_dict = nx.degree_centrality(graph)
     print('Degree Centrality: ', max(centrality_dict,
-                                     key=centrality_dict.get), max(centrality_dict.values()))
-    centrality_dict = bipartite.betweenness_centrality(graph, node_set)
+                                    key=centrality_dict.get), max(centrality_dict.values()))
+    centrality_dict = nx.in_degree_centrality(graph)
+    print('In Degree Centrality: ', max(centrality_dict,
+                                    key=centrality_dict.get), max(centrality_dict.values()))
+    centrality_dict = nx.out_degree_centrality(graph)
+    print('Out Degree Centrality: ', max(centrality_dict,
+                                    key=centrality_dict.get), max(centrality_dict.values()))
+    centrality_dict = nx.eigenvector_centrality_numpy(graph)
+    print('Eigenvector Centrality: ', max(centrality_dict,
+                                    key=centrality_dict.get), max(centrality_dict.values()))
+    centrality_dict = nx.katz_centrality(graph)
+    print('Katz Centrality: ', max(centrality_dict,
+                                    key=centrality_dict.get), max(centrality_dict.values()))
+    centrality_dict = nx.closeness_centrality(graph)
+    print('Closeness Centrality: ', max(centrality_dict,
+                                    key=centrality_dict.get), max(centrality_dict.values()))
+    centrality_dict = nx.betweenness_centrality(graph)
     print('Betweenness Centrality: ', max(centrality_dict,
-                                          key=centrality_dict.get), max(centrality_dict.values()))
+                                    key=centrality_dict.get), max(centrality_dict.values()))
 
 
-# Plot bipartite degree distributions
-def plot_degree_distributions(graph, node_set):
+# Plot degree distributions
+def plot_degree_distributions(graph, social_nodes, technical_nodes):
 
-    # Get set of nodes
-    social, technical = bipartite.degrees(graph, node_set)
-
+    # Get degrees for subset
+    social_deg = nx.degree(graph, social_nodes)
+    technical_deg = nx.degree(graph, technical_nodes)
+    
     # Extract values only
-    social = [lis[1] for lis in social]
-    technical = [lis[1] for lis in technical]
-    sociotechnical = social + technical
+    social_deg = [lis[1] for lis in social_deg]
+    technical_deg = [lis[1] for lis in technical_deg]
+    sociotechnical_deg = social_deg + technical_deg
 
-    print('Social Nodes: ', len(social))
-    print('Technical Nodes: ', len(technical))
+    print('Social Nodes: ', len(social_deg))
+    print('Technical Nodes: ', len(technical_deg))
 
     # Weights used to normalize the distribution such that bars add to 1
-    social_weights = np.ones_like(social) / len(social)
-    technical_weights = np.ones_like(technical) / len(technical)
-    sociotechnical_weights = np.ones_like(sociotechnical) / len(sociotechnical)
+    social_weights = np.ones_like(social_deg) / len(social_deg)
+    technical_weights = np.ones_like(technical_deg) / len(technical_deg)
+    sociotechnical_weights = np.ones_like(sociotechnical_deg) / len(sociotechnical_deg)
 
-    plt.hist(social, weights=social_weights)
+    plt.hist(social_deg, weights=social_weights)
     plt.title('Social Degree Distribution')
     plt.xlabel('k')
     plt.ylabel('p(k)')
     plt.show()
 
-    plt.hist(technical, weights=technical_weights)
+    plt.hist(technical_deg, weights=technical_weights)
     plt.title('Technical Degree Distribution')
     plt.xlabel('k')
     plt.ylabel('p(k)')
     plt.show()
 
-    plt.hist(sociotechnical, weights=sociotechnical_weights)
+    plt.hist(sociotechnical_deg, weights=sociotechnical_weights)
     plt.title('Sociotechnical Degree Distribution')
     plt.xlabel('k')
     plt.ylabel('p(k)')
@@ -115,26 +125,24 @@ def plot_degree_distributions(graph, node_set):
 
 def visualize_network(graph, social_nodes, technical_nodes):
     
+    
     # Need to create subset of technical node labels
     # Not showing social node labels
     labels = {}
     for node in graph.nodes():
         if node in technical_nodes:
             labels[node] = node
-
-    # Formats graph layout
-    # k equation is optimal spacing between nodes
-    pos = nx.spring_layout(
-        graph, k=.3*1/np.sqrt(len(graph.nodes())), iterations=20)
-
+    
+    pos = nx.kamada_kawai_layout(graph)
+    
     # Draw nodes, edges, and labels
     nx.draw_networkx_nodes(graph, pos, nodelist=technical_nodes,
                            node_size=500, node_color='cyan', node_shape='s',
                            edgecolors='black', label='repository')
     nx.draw_networkx_nodes(graph, pos, nodelist=social_nodes, alpha=.5,
-                           node_size=15, node_color='blue', node_shape='o',
+                           node_size=25, node_color='purple', node_shape='o',
                            edgecolors='black', label='Developer')
-    nx.draw_networkx_edges(graph, pos, alpha=.5, edge_color='purple')
+    nx.draw_networkx_edges(graph, pos, alpha=.5, edge_color='black')
     nx.draw_networkx_labels(graph, pos, labels,
                             font_size=5, font_weight='bold',
                             font_color='black')
