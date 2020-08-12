8735c2506f6acba4204307cbde413fb4b542bb03
pacpix <stachofskyjustin@gmail.com>
2020-08-12 16:45:45 -0700

    Added R igraph files used to analyze the expanded network

---
+++ Final_Project/network_analysis_igraph.R
@@ -0,0 +1,29 @@
+library(igraph)
+library (qgraph)
+library(zoom)
+G=read.graph("st_graph.gml","gml")
+
+e <- get.edgelist(G)
+l <- qgraph.layout.fruchtermanreingold(e,vcount=vcount(G),area=9*(vcount(G)^2),repulse.rad=(vcount(G)^3.1))
+PageWeights=max(E(G)$weight+1)-E(G)$weight #pagerank uses affinity rather than distance weights on edges
+
+
+V(G)[V(G)$repo==1]$shape <- "square"
+V(G)[V(G)$repo==1]$color <- "cyan"
+V(G)[V(G)$repo==1]$size <- 10
+V(G)[V(G)$repo==0]$shape <- "circle"
+V(G)[V(G)$repo==0]$color <- "purple"
+V(G)[V(G)$repo==0]$size <- 5
+
+
+plot(G,
+     vertex.frame.color = "black", #set vertex border color
+     vertex.label = NA,
+     edge.arrow.size=.1,
+     edge.arrow.size=.1,
+     edge.color = "black", #set edge color
+     layout=l
+)
+
+zm()
+
