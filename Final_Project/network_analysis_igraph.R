1d602230a9848559a8627a81a4ede562dcd8782b
Julia Stachofsky <julia_stachofsky@protonmail.com>
2022-10-27 23:16:26 -0700

    R file modifications and added diagrams

--- Final_Project/network_analysis_igraph.R
+++ Final_Project/network_analysis_igraph.R
@@ -10,36 +10,37 @@ PageWeights=max(E(G)$weight+1)-E(G)$weight #pagerank uses affinity rather than d
 clusters <- cluster_walktrap(G)$membership 
 
 # Community statistics
-modularity(clusters)
-membership(clusters)
+modularity(wc)
+membership(wc)
 
 colbar <- rainbow(max(clusters)+1)
 V(G)$color <- colbar[clusters+1]
+V(G)[V(G)$repo==1]$shape <- "square"
+V(G)[V(G)$repo==0]$shape <- "circle"
 
-#to set node sizes
+# Large Graph Plot
 plot(G, 
      vertex.frame.color = "black", # set vertex border color
      vertex.label = NA,
-     edge.arrow.size=.01,
+     edge.arrow.mode=0,
+     edge.width=.01,
      vertex.size=(page.rank(G)$vector+0.012)/max(page.rank(G)$vector)*7.5,
      edge.color = "black", # set edge color
      layout=l
 )
 
 
-# Original graph plot
+# Small graph plot
 V(G)[V(G)$repo==1]$shape <- "square"
 V(G)[V(G)$repo==1]$color <- "cyan"
 V(G)[V(G)$repo==1]$size <- 10
 V(G)[V(G)$repo==0]$shape <- "circle"
 V(G)[V(G)$repo==0]$color <- "purple"
-V(G)[V(G)$repo==0]$size <- 5
+V(G)[V(G)$repo==1]$size <- 5
 
 plot(G,
      vertex.frame.color = "black", # set vertex border color
-     vertex.label = NA,
-     edge.arrow.size=.1,
-     edge.arrow.size=.1,
+     edge.arrow.size=.2,
      edge.color = "black", # set edge color
      layout=l
 )
